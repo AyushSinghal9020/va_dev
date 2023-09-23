8fb74b7a02a823dfa4e0ba0a6f442930e24b910e
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-09-23 15:34:35 +0530

    Update va_dev.py
    # <--------------Find Iter-------------->
    What : re_find now uses re.finditer instead of re.findall
    Why : Puts `tuple`/`dictionary`/`list`/`any other iterable` into one scope 
    # <--------------Find Iter-------------->
    What : vdev now supports C++ codes with multiple returns
    Why : Adds feasibility working with various languages

--- va_dev/src/va_dev.py
+++ va_dev/src/va_dev.py
@@ -1,10 +1,11 @@
 import re
 import os 
 import subprocess
+import warnings
 
-class va_dev:
+class v_dev:
 
-    def __init__(self , load_type : str):
+    def __init__(self , load_type = 'c++'):
         '''
             Intializizes the object
 
@@ -14,6 +15,11 @@ class va_dev:
         '''
 
         self.load_type = load_type
+
+    def convert_to_list(self , val):
+
+        if isinstance(val , list): return val
+        else : return [val]
 
     def re_match(self , value : str , expressions : list) -> list:
         '''
@@ -33,7 +39,11 @@ class va_dev:
 
                 match = re.match(exp , val)
 
-                if match: matches.append(match.group())
+                if match:
+
+                    matches = self.convert_to_list(matches) 
+                    
+                    matches.append(match.group())
 
         return matches
     
@@ -47,7 +57,7 @@ class va_dev:
             2) expressions - List of expressions to look for in `value` 
         '''
 
-        matches = [re.findall(exp , value)
+        matches = [re.finditer(exp , value)
                    for exp 
                    in expression]
 
@@ -55,39 +65,46 @@ class va_dev:
 
         for f_val in matches:
             for s_val in f_val:
-                r_matches.append(s_val)
+                r_matches.append(s_val.group())
         
         return set(r_matches)
 
     
-    def load_lib(self, path : str , extra_returns = [] , ignore_main = 'False'):
+    def load_lib(self, path : str , extra_returns = [] , ignore_main = False , func_regex = [] , class_regex = []):
         '''
             Loads the file into the memory. Search for functions/classes and make seperate Dictionaries to load
 
             Args - 
 
             1) path - Absolute Path to the file 
-            2)  extra_returns - List of extra return types defined in function example `tuple func_name(){\\func_body}`
+            2) extra_returns - List of extra return types defined in function example `tuple func_name(){\\func_body}`
+            3) ignore_main - Ignore the main function or not 
+            4) func_regex - Extra regular expression for function
+            5) class_regex - Extra regular expression for class
         '''
         
         self.path = path
         self.extra_returns = extra_returns
+        self.func_regex = func_regex
+        self.class_regex = class_regex
 
 
         with open(self.path , 'r') as program: self.content = program.read()
 
         if self.load_type == 'c++':
             
-            self.lib_patterns = [r'#include\s+[<"]?([^<>"]+)[<"]?>' , 
-                                 r'# include\s+[<"]?([^<>"]+)[<"]?>']
-            self.func_patterns = [r'int\s+\w+\s*\([^)]*\)\s*{[^}]*}' , 
-                                  r'void\s+\w+\s*\([^)]*\)\s*{[^}]*}']
-                                #   r'char\s+\w+\s*\([^)]*\)\s*{[^}]*}']
-            self.class_patterns = [r'class\s+\w+\s*\([^)]*\)\s*{[^}]*}']
+            self.lib_patterns = [r'#include\s+[<"]?([^<>"]+)[<"]?>' ,
+                                 r'#include \s+[<"]?([^<>"]+)[<"]?>' 
+                                 r'# include\s+[<"]?([^<>"]+)[<"]?>' , 
+                                 r'# include \s+[<"]?([^<>"]+)[<"]?>']
+            self.identifier_pattern = [r'using\s[^;]+;']
+            self.func_patterns = [r'int\s+[a-zA-Z_][a-zA-Z_0-9]*\s*\([^)]*\)\s*{[^}]*}' , 
+                                  r'void\s+[a-zA-Z_][a-zA-Z_0-9]*\s*\([^)]*\)\s*{[^}]*}'] + self.func_regex
+            self.class_patterns = [r'class\s+[a-zA-Z_][a-zA-Z_0-9]*\s*\([^)]*\)\s*{[^}]*}'] + self.class_regex
             
             if self.extra_returns: 
                 
-                self.ex_funcs = [extra_return + '\s+\w+\s*\([^)]*\)\s*{[^}]*}'
+                self.ex_funcs = [extra_return + '\s+[a-zA-Z_][a-zA-Z_0-9]*\s*\([^)]*\)\s*{[^{}]*((\{[^{}]*\})*[^{}]*)*}'
                                  for extra_return
                                  in extra_returns]
                 
@@ -97,7 +114,11 @@ class va_dev:
            
             self.libs = self.re_match(self.value , self.lib_patterns)
             self.s_libs = ''
-            for val in self.libs:self.s_libs += val + '\n'
+            for val in self.libs: self.s_libs += val + '\n'
+            
+            self.identifiers = self.re_find(self.content , self.identifier_pattern)
+            self.s_identifiers = ''
+            for val in self.identifiers: self.s_identifiers += val + '\n'
             
             self.funcs = self.re_find(self.content , self.func_patterns)
             self.classes = self.re_find(self.content , self.class_patterns)
@@ -144,39 +165,65 @@ class va_dev:
 
         return arguments 
 
-    def execute(self , file_name):
+    def execute(self , file_name : str):
 
         if self.load_type == 'c++':
 
             os.system(f'g++ {file_name}')
 
             try : r = subprocess.run([f'{self.func_name}.exe'])
-            except Exception as e : 
-                try : r = subprocess.run(['a.exe'])
-                except Exception as e : r = subprocess.run(['a.out'])
+            except Exception as e : r = subprocess.run(['a.exe'])
 
             return r
 
-    def load_func(self , func_name , args = []):
+    def load_func(self , func_name : str , args = [] , multiple_returns = False , multiple_args = None):
         '''
             Loads the function into existance. Creates the function files and Run them as per the arguments
 
             Args - 
             1) func_name - Name of the function to be loaded
             2) args - List of arguments to be passed in 
+            3) multiple_returns - If the function returns multiple values. Set this to True
         '''
 
         self.func_name = func_name
         self.args = args
+        self.multiple_returns = multiple_returns
+        self.multiple_args = multiple_args
+
+        if self.args == []:warnings.warn('''args is passed empty. 
+                                         This can lead to not making of the exe file if the function requires arguments. 
+                                         If the function do not require arguments Please ignore this warning''')
 
         if self.load_type == 'c++':
+
+            self.multiple_args_line = ''
+
+            if self.multiple_returns :
+                self.s_libs += '\n#include <fstream>' 
+                self.write_func = '''
+void WriteResultToFile(const vector<int>& data) {
+    ofstream outputFile("output.txt");
+    if (outputFile.is_open()) {
+        for (int value : data) {outputFile << value << " ";}
+        outputFile.close();}
+    else {cerr << "Error: Unable to open the output file." << endl;}}'''
+                
+                self.main_func = 'int main(){\n\t' + f'{self.multiple_args} result = {func_name}({self.arg_builder(self.args)});\n' +f'\t WriteResultToFile(result);\n' + '\t return 0;\n}'
+                self.write_up = f'{self.s_libs}\n{self.s_identifiers}\n{self.func_desc[self.func_name]}\n{self.write_func}\n{self.main_func}'
             
-            self.main_func = 'int main(){\n\t' + f'{func_name}({self.arg_builder(self.args)});\n' + '\t return 0;\n}'
-            self.write_up = f'{self.s_libs}\n{self.func_desc[self.func_name]}\n{self.main_func}' 
+            else : 
+                self.main_func = 'int main(){\n\t' + f'{func_name}({self.arg_builder(self.args)});\n' + '\t return 0;\n}'
+                self.write_up = f'{self.s_libs}\n{self.s_identifiers}\n{self.func_desc[self.func_name]}\n{self.main_func}' 
         
             with open (f'{self.func_name}.cpp' , 'w') as func_program: func_program.write(self.write_up)
 
             return_val = self.execute(file_name = f'{self.func_name}.cpp')
+
+            if self.multiple_args:
+                with open('output.txt' , 'r') as output : return_s = output.read()
+
+                return_val = return_s.split()
 
             return return_val
 
